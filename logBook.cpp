22c2e7b7262bc14f0c80e6cb903fbf7494f12b5d
Ben Holden <bholden1@andrew.cmu.edu>
2015-11-17 11:34:06 -0500

    Modified log reader

--- logBook.cpp
+++ logBook.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <stdlib.h>
 
 #define NUM_RANGES 180
 
@@ -21,42 +22,68 @@ int import_logs(const char *logName, vector<logEntry> & logBook) {
 	logEntry logData;
 	char debugType;
 	istringstream stringin(logLine);
-	if(logLine[0] == 'O') {
-	    logData.logType = ODOM;
-	    stringin >> debugType;
-	    stringin >> logData.robotPose.x;
-	    stringin >> logData.robotPose.y;
-	    stringin >> logData.robotPose.theta;
-	    stringin >> logData.ts;
+	
+	string buf;
+	vector<string> tokens;
+	while(stringin >> buf) {
+	    tokens.push_back(buf);
 	}
-	else if(logLine[0] == 'L') {
-	    logData.logType = LIDAR;
-	    stringin >> debugType;
-	    stringin >> logData.robotPose.x;
-	    stringin >> logData.robotPose.y;
-	    stringin >> logData.robotPose.theta;
-	    stringin >> logData.lidarPose.x;
-	    stringin >> logData.lidarPose.y;
-	    stringin >> logData.lidarPose.theta;
-	    fprintf(stderr, "ERROR 3 %f\n",logData.lidarPose.theta);
-	    for(unsigned int i = 0; i < NUM_RANGES; i++) {
-		fprintf(stderr, "ERROR range number %i\n",i);
-		stringin >> logData.ranges[i];
-		fprintf(stderr, "ERROR range value %f\n",logData.ranges[i]);
+	
+	for(int i = 0; i < tokens.size(); i++)
+	{
+	    if(i == 0)
+	    {
+		char type = tokens.at(0)[0];
+		if(type == 'O')
+		    fprintf(stderr, "ODOM\t");
 	    }
-	    stringin >> logData.ts;
-	    fprintf(stderr, "ERROR 3 TS_END\n");
+	    if(i == 1)
+	    {
+		float x_test = strtof(tokens.at(i).c_str(), NULL);
+		fprintf(stderr, "x is %f\t", x_test);
+	    }
 	}
-	else {
-	    fprintf(stderr, "BAD LOG FILE: %s Line: %lu\n", logName, logBook.size());
-	    return -1;
-	}
-	fprintf(stderr, "ERROR 4\n");
-	logBook.push_back(logData);
-	fprintf(stderr, "ERROR 5\n");
-	cout << logBook.back().logType << " " << logBook.back().robotPose.x << " ";
-	cout << logBook.back().robotPose.y << " " << logBook.back().ts << endl;
-	fprintf(stderr, "ERROR 6\n");
+	float ts = strtof(tokens.back().c_str(), NULL);
+	fprintf(stderr, "ts is %f\n", ts);
+	
+	
+	
+// 	if(logLine[0] == 'O') {
+// 	    logData.logType = ODOM;
+// 	    stringin >> debugType;
+// 	    stringin >> logData.robotPose.x;
+// 	    stringin >> logData.robotPose.y;
+// 	    stringin >> logData.robotPose.theta;
+// 	    stringin >> logData.ts;
+// 	}
+// 	else if(logLine[0] == 'L') {
+// 	    logData.logType = LIDAR;
+// 	    stringin >> debugType;
+// 	    stringin >> logData.robotPose.x;
+// 	    stringin >> logData.robotPose.y;
+// 	    stringin >> logData.robotPose.theta;
+// 	    stringin >> logData.lidarPose.x;
+// 	    stringin >> logData.lidarPose.y;
+// 	    stringin >> logData.lidarPose.theta;
+// 	    fprintf(stderr, "ERROR 3 %f\n",logData.lidarPose.theta);
+// 	    for(unsigned int i = 0; i < NUM_RANGES; i++) {
+// 		fprintf(stderr, "ERROR range number %i\n",i);
+// 		stringin >> logData.ranges[i];
+// 		fprintf(stderr, "ERROR range value %f\n",logData.ranges[i]);
+// 	    }
+// 	    stringin >> logData.ts;
+// 	    fprintf(stderr, "ERROR 3 TS_END\n");
+// 	}
+// 	else {
+// 	    fprintf(stderr, "BAD LOG FILE: %s Line: %lu\n", logName, logBook.size());
+// 	    return -1;
+// 	}
+// 	fprintf(stderr, "ERROR 4\n");
+// 	logBook.push_back(logData);
+// 	fprintf(stderr, "ERROR 5\n");
+// 	cout << logBook.back().logType << " " << logBook.back().robotPose.x << " ";
+// 	cout << logBook.back().robotPose.y << " " << logBook.back().ts << endl;
+// 	fprintf(stderr, "ERROR 6\n");
     }
     log.close();
     return 1;
